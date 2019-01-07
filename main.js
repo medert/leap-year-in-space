66c08693bc3b6e98a12bfb0b44ec0ecb934518ab
Meder <m.toktosunov@icloud.com>
2019-01-07 14:56:18 -0500

    added .printLeapInCurrentCentury()

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
 class Year {
-  // your code, here
+  constructor(year){
+    this.year = year;
+  }
+  isLeapYear(){
+    if (this.year % 4 !== 0){
+      return false;
+    } else if (this.year % 100 !== 0){
+      return true;
+    } else if (this.year % 400 !== 0){
+      return false;
+    } else {
+      return true;
+    }
+  }
+  printLeap(){
+
+    this.findCentury();
+
+    let i = this.year;
+    for (this.year; this.year <= (i + 100); ++this.year ){
+      if (this.isLeapYear()){
+        console.log(this.year);
+      }
+    }
+  }
+
+  findCentury(){
+    if (this.year <= 100){
+      this.year = 0;
+    } else {
+      this.year = this.year - this.year % 100;
+    }
+  }
 }
+
+let currentYear = new Year(2017);
+
+console.log(currentYear.isLeapYear());
+console.log(currentYear.printLeap());
